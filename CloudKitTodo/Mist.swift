f05b4485245106c2f5da6387ff000155e6fe9909
Matthew McCroskey <matthew.mccroskey@gmail.com>
2016-12-01 15:25:19 -0600

    Add Mist class to project.

---
+++ CloudKitTodo/Mist.swift
@@ -0,0 +1,18 @@
+//
+//  Mist.swift
+//  CloudKitTodo
+//
+//  Created by Matthew McCroskey on 12/1/16.
+//  Copyright © 2016 Less But Better. All rights reserved.
+//
+
+import Foundation
+
+class Mist {
+    
+    
+    // MARK: - Private Properties
+    
+    private static let operationQueue = OperationQueue()
+    
+}
