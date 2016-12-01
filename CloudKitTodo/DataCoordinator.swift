0f236f8229d1efe655cc3e1b70b916c9646e6093
Matthew McCroskey <matthew.mccroskey@gmail.com>
2016-11-30 21:37:52 -0600

    Implement the pushLocalChanges() function of DataCoordinator.

--- CloudKitTodo/DataCoordinator.swift
+++ CloudKitTodo/DataCoordinator.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CloudKit
 
-typealias RefreshCompletion = ((Bool, Error?) -> Void)
+typealias RefreshCompletion = (([CKDatabaseScope:Bool], Bool, Error?) -> Void)
 
 class DataCoordinator {
     
@@ -87,7 +87,127 @@ class DataCoordinator {
     
     // MARK: - Updating Remote Content with Changes from Local
     
-    
+    func pushLocalChanges(_ completion:RefreshCompletion) {
+        
+        let scopes: [CKDatabaseScope] = [.public, .shared, .private]
+        
+        let unpushedChanges = self.localStorageInterface.changedRecordsAwaitingPushToCloud
+        let unpushedDeletions = self.localStorageInterface.deletedRecordsAwaitingPushToCloud
+        
+        var unpushedChangesDictionary: [CKDatabaseScope : [CKRecord]] = [:]
+        var idsOfUnpushedDeletionsDictionary: [CKDatabaseScope : [CKRecordID]] = [:]
+        
+        // Gather up all the unpushed changes and deletions and group them by database scope
+        var counter = 0
+        while counter < scopes.count {
+            
+            let scope = scopes[counter]
+            
+            let unpushedChangesForCurrentAccessibility = unpushedChanges.filter({ $0.accessibility == scope }).map({ $0.backingRemoteRecord })
+            unpushedChangesDictionary[scope] = unpushedChangesForCurrentAccessibility
+            
+            let idsOfUnpushedDeletionsForCurrentAccessibility = unpushedDeletions.filter({ $0.accessibility == scope }).map({ CKRecordID(recordName: $0.identifier) })
+            idsOfUnpushedDeletionsDictionary[scope] = idsOfUnpushedDeletionsForCurrentAccessibility
+            
+            counter = counter + 1
+            
+        }
+        
+        var modifyOperations: [CKDatabaseScope : CKModifyRecordsOperation] = [:]
+        var finishedStates: [CKDatabaseScope : Bool] = [
+            
+            .public : false,
+            .shared : false,
+            .private : false
+            
+        ]
+        
+        // Create a modify operation for each database scope
+        for scope in scopes {
+            
+            let recordsToSave = unpushedChangesDictionary[scope]
+            let recordIdsToDelete = idsOfUnpushedDeletionsDictionary[scope]
+            
+            let modifyOperation = CKModifyRecordsOperation(recordsToSave: recordsToSave, recordIDsToDelete: recordIdsToDelete)
+            modifyOperation.modifyRecordsCompletionBlock = { (savedRecords, recordIDsOfDeletedRecords, operationError) in
+                
+                // Mark this database's modify operation as complete
+                finishedStates[scope] = true
+                
+                // If there's an error, then return it and bail out of everything 
+                // (since the operations have a linear dependency, bailing out of
+                // a particular operation bails out of any that follow)
+                if let operationError = operationError {
+                    completion(finishedStates, false, operationError)
+                    return
+                }
+                
+                // If this is the last of the three operations
+                if scope == .private {
+                    completion(finishedStates, true, nil)
+                }
+                
+            }
+            
+        }
+        
+        func dictionaryKeysMismatchFatalError(_ name:String, dictionary:[CKDatabaseScope:Any]) -> Never {
+            
+            fatalError(
+                "The keys for the \(name) dictionary and the scopes dictionary must match, " +
+                    "but they don't. Here are those dictionaries:\n" +
+                    "\(name): \(dictionary)\n" +
+                    "scopes: \(scopes)\n"
+            )
+            
+        }
+        
+        // Make each modify operation dependent upon the previous database scope
+        counter = (scopes.count - 1)
+        while counter > 0 {
+            
+            let currentScope = scopes[counter]
+            guard let currentModifyOperation = modifyOperations[currentScope] else {
+                dictionaryKeysMismatchFatalError("modifyOperations", dictionary: modifyOperations)
+            }
+            
+            let previousScope = scopes[counter - 1]
+            guard let previousModifyOperation = modifyOperations[previousScope] else {
+                dictionaryKeysMismatchFatalError("modifyOperations", dictionary: modifyOperations)
+            }
+            
+            currentModifyOperation.addDependency(previousModifyOperation)
+            
+            counter = counter - 1
+            
+        }
+        
+        let container = CKContainer.default()
+        let databases: [CKDatabaseScope : CKDatabase] = [
+            
+            .public : container.publicCloudDatabase,
+            .shared : container.sharedCloudDatabase,
+            .private : container.privateCloudDatabase
+            
+        ]
+        
+        // Add each modify operation to its respective database's operation queue
+        for scope in scopes {
+            
+            guard let database = databases[scope] else {
+                dictionaryKeysMismatchFatalError("databases", dictionary: databases)
+            }
+            
+            guard let modifyOperation = modifyOperations[scope] else {
+                dictionaryKeysMismatchFatalError("modifyOperations", dictionary: modifyOperations)
+            }
+            
+            database.add(modifyOperation)
+            
+        }
+        
+        
+    }
     
     
     // MARK: - Private Functions
